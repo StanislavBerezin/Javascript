42026e4e4165e3a9af715dc459a4bc0cc5e4b3b8
Ubuntu <stanislav_berezin@hotmail.com>
2018-11-26 22:16:33 +1000

    added more in prototype inheritance

--- Advanced/prototype_inheritence.js
+++ Advanced/prototype_inheritence.js
@@ -15,3 +15,30 @@ console.log(son_2.getArg)
 
 // there is a chain of prototype objects, our object, then parent object, 
 // then lastly is the master object which contains everything, thats how js operates
+
+
+// 
+// Base class
+let Job = function(){
+    this.pays = true
+}
+
+Job.prototype.print = function(){
+    console.log(this.pays ? "Hire me" : "No thanks")
+}
+
+// Subclass
+let TechJob = function(title, pays){
+    // this referes to this TechJob functions, and its assigned to Job class
+    Job.call(this)
+    this.title = title,
+    this.pays = pays
+}
+// now it inherits the Job prototype functions
+TechJob.prototype = Object.create(Job.prototype)
+// constructor
+TechJob.prototype.constructor = TechJob
+
+let softwarePosition = new TechJob("Javascript", true)
+
+console.log(softwarePosition.print())
